4e603b046cb8d3fe6a1f593188f722fe87342a42
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-11-24 19:55:43 -0500

    Added comments

--- src/grumble.h
+++ src/grumble.h
@@ -41,22 +41,48 @@ namespace GRUMBLE
 	class Node
 	{
 		public:
+			// Represents whether the node is the start or end
 			bool start;
 			bool end;
+
+			// Sets a recursion limit
+			int limit;
+
+			// The character the Node is set to capture
+			int capture;
+
+			// The character the Node will test against.
+			int given;
+
+			// Contains all the pointers to other Nodes
 			std::vector <Node*> connections;
-			Node();
+
+			// Constructor
+			Node(int, int);
+
+			// Function that adds a connection
+			Node* addConnection(int, Node*);
 	};
 
-	Node::Node()
+	Node::Node(int capture, int limit = 0)
 	{
 		this -> start = false;
 		this -> end = false;
+		this -> limit = limit;
+		this -> capture = capture;
+		
+		// Load up all the connections as NULL
 		int i;
 		for(i = 0; i < 128; i++)
 		{
-			Node* next;
-			this -> connections.push_back(next);
+			this -> connections.push_back(NULL);
 		}
+	}
+
+	Node* Node::addConnection(int index, Node* node)
+	{
+		this -> connections[index] = node;
+		return node;
 	}
 
 }
